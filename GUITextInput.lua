1b96117e1002a83d382271373260bbe8bee15401
funknapkin <funknapkin@gmail.com>
2015-07-26 09:00:42 -0400

    Display some default rectangle and text when adding a user to the blacklist. This is mostly to test the drawing functions, the actual logic will come when I've fixed the remaining issues.

--- GUITextInput.lua
+++ GUITextInput.lua
@@ -16,6 +16,7 @@ if not GUITextInput then
     local kb = Input:keyboard()
     self.workspace:connect_keyboard(kb)
     self.panel = self.workspace:panel({ name = "workspace_panel" })
+    self.gui_items = {}
 
     -- Set the callbacks on keyboard events
     self.panel:enter_text(function(that, char) self:on_enter_text(char) end)
@@ -24,10 +25,15 @@ if not GUITextInput then
     -- Inject code in the Payday 2 managers to prevent key presses from processing
     self:_inject_hooks()
 
-    -- TODO: show a panel that displays the text entered
+    -- Show the text input UI
+    self:_draw_ui()
+  end
 
-    -- TODO: prevent key presses from affecting the rest of the interface
-    --       (mostly esc/enter, but should block all inputs)
+  function GUITextInput:delete()
+  	for _, child in ipairs(self.panel:children()) do
+  		self.panel:remove(child)
+  	end
+  	self.panel:parent():remove(self._panel)
   end
 
   function GUITextInput:on_input_complete(canceled)
@@ -79,6 +85,74 @@ if not GUITextInput then
 
   function GUITextInput:_remove_hooks()
     managers.menu.update = self.orig_update
+  end
+
+  function GUITextInput:_draw_ui()
+  -- Show a rectangle with some default text on it, to test the display functions
+  -- Known issue: everything is drawn under the Payday menu
+  -- TODO: use "const" variables to define size, position, etc.
+  -- TODO: get the game's resolution and adjust display
+  self.gui_items.background = self.gui_items.background or self.panel:rect({
+    name = "bg",
+    x = 660,
+    y = 340,
+    w = 600,
+    h = 400,
+    blend_mode = "normal",
+    color = Color.black,
+    layer = 100 })
+  self.gui_items.left_border = self.gui_items.left_border or self.panel:rect({
+    name = "left_border",
+    x = 660,
+    y = 340,
+    w = 3,
+    h = 400,
+    blend_mode = "normal",
+    color = Color.white,
+    layer = 100 })
+  self.gui_items.right_border = self.gui_items.right_border or self.panel:rect({
+    name = "right_border",
+    x = 1260 - 3,
+    y = 340,
+    w = 3,
+    h = 400,
+    blend_mode = "normal",
+    color = Color.white,
+    layer = 100 })
+  self.gui_items.top_border = self.gui_items.top_border or self.panel:rect({
+    name = "top_border",
+    x = 660,
+    y = 340,
+    w = 600,
+    h = 3,
+    blend_mode = "normal",
+    color = Color.white,
+    layer = 100 })
+  self.gui_items.bottom_border = self.gui_items.bottom_border or self.panel:rect({
+    name = "bottom_border",
+    x = 660,
+    y = 740 - 3,
+    w = 600,
+    h = 3,
+    blend_mode = "normal",
+    color = Color.white,
+    layer = 100 })
+  self.gui_items.header = self.gui_items.header or self.panel:text({
+    name = "header",
+    x = 0,
+    y = 340 + 10,
+    h = tweak_data.menu.pd2_small_font_size * 1.5,
+    align = "center",
+    vertical = "center",
+    color = Color.white,
+    layer = 101,
+    font = tweak_data.menu.pd2_small_font,
+    font_size = tweak_data.menu.pd2_small_font_size * 1.25 })
+  self.gui_items.header:set_text("Test header")
+
+  -- TODO: show a panel that displays the text entered
+  -- Should have 3 text areas: the header, the prompt, the input area
+  -- Note: verify that text wrapping works for the input area
   end
 
 end
